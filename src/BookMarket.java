52dbe0b65fc996cbf13a3aa6ec0045d34ed91d29
mushrimp0406 <60537456+mushrimp0406@users.noreply.github.com>
2024-03-12 14:46:06 +0900

    level 1 끝

---
+++ src/BookMarket.java
@@ -0,0 +1,10 @@
+
+public class BookMarket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to ShoppingMall!");
+		System.out.println("Welcome to BookMarket!!");
+	}
+
+}
